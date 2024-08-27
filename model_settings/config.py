d6723615c721aa8d6290afbaf5abddef16ef5a8b
Seneca <www.iamsayourah@gmail.com>
2024-08-27 11:29:43 -0400

    removed English dataset, I changed my mind on what to build

--- model_settings/config.py
+++ model_settings/config.py
@@ -1,15 +1,6 @@
 import torch
 
-DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-BATCH_SIZE = 128
-NUM_EPOCHS = 15
-LEARNING_RATE = 1e-3
-TRAIN_IMAGES_DIR = 'datasets/train_v2/train'
-VALID_IMAGES_DIR = 'datasets/validation_v2/validation'
-TEST_IMAGES_DIR = 'datasets/test_v2/test'
-TRAIN_CSV = 'datasets/written_name_train_v2.csv'
-VALID_CSV = 'datasets/written_name_validation_v2.csv'
-TEST_CSV = 'datasets/written_name_test_v2.csv'
-MAX_LENGTH = 20
-VOCAB_SIZE = 32
-IMAGE_SIZE = [300, 100]
+device = "cuda" if torch.cuda.is_available() else "cpu"
+batch_size = 128
+epochs = 15
+learning_rate = 1e-3
